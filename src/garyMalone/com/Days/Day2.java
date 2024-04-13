062ed14dedf9c2d82b388bdf92a845035e173c47
Gary <gazyellow2003@googlemail.com>
2024-04-13 01:51:29 +0100

    Day 2 part 1

---
+++ src/garyMalone/com/Days/Day2.java
@@ -0,0 +1,28 @@
+package garyMalone.com.Days;
+
+import garyMalone.com.Objects.CubeGame;
+public class Day2 {
+	
+	private static final int MAX_GREEN = 13;
+	private static final int MAX_RED = 12;
+	private static final int MAX_BLUE = 14;
+	
+	public static int part1(final String[] input) {
+		int total = 0;
+		for(String str : input) {
+			str = str.trim();
+			CubeGame game = new CubeGame();
+			game.populateCubeGame(str);
+			if(game.isGamePossible(MAX_GREEN, MAX_RED, MAX_BLUE)) {
+				total += game.getId();
+			}
+		}
+		
+		return total;
+	}
+	
+	public static void part2(final String[] input) {
+		
+	}
+	
+}
---
+++ src/garyMalone/com/Objects/CubeGame.java
@@ -0,0 +1,92 @@
+package garyMalone.com.Objects;
+
+public class CubeGame {
+	
+	public int id;
+	public int maximumGreen = 0;
+	public int maximumRed 	= 0;
+	public int maximumBlue	= 0;
+	
+	public void populateCubeGame(String input) {
+		String gameString[] = input.split(":");
+		parseGameId(gameString[0]);
+		parseMaxCubes(gameString[1]);
+	}
+	
+	public void parseGameId(String str){
+		 setId(Integer.parseInt(str.replaceAll("[^\\d]", "")));
+	}
+	
+	public void parseMaxCubes(String str) {
+		String cubeGames[] = str.split(";");
+		for(String cubeGame : cubeGames) {
+			String cubeStrings[] = cubeGame.split(",");
+			for(String cubeStr : cubeStrings) {
+				int number = Integer.parseInt(cubeStr.replaceAll("[^\\d]", ""));
+				if(cubeStr.toLowerCase().endsWith("green") && isNumberGreaterThanGreen(number)) {
+					this.maximumGreen = number;
+				}
+				else if(cubeStr.toLowerCase().endsWith("red") && isNumberGreaterThanRed(number)) {
+					this.maximumRed = number;
+				}
+				else if(cubeStr.toLowerCase().endsWith("blue") && isNumberGreaterThanBlue(number)) {
+					this.maximumBlue = number;
+				}
+			}
+		}
+	}
+	
+	private boolean isNumberGreaterThanGreen(int number) {
+		return number > maximumGreen;
+	}
+	
+	private boolean isNumberGreaterThanRed(int number) {
+		return number > maximumRed;
+	}
+	
+	private boolean isNumberGreaterThanBlue(int number) {
+		return number > maximumBlue;
+	}
+	
+	public boolean isGamePossible(int allowedGreen, int allowedRed, int allowedBlue) {
+		return this.maximumGreen <= allowedGreen && this.maximumRed <= allowedRed && this.maximumBlue <= allowedBlue;
+	}
+	
+	public void printCubes() {
+		System.out.println("game " + this.id + ": red: " + this.maximumRed + " blue: " + this.maximumBlue + " green: " + this.maximumGreen);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getMaximumGreen() {
+		return maximumGreen;
+	}
+	
+	public void setMaximumGreen(int maximumGreen) {
+		this.maximumGreen = maximumGreen;
+	}
+	
+	public int getMaximumRed() {
+		return maximumRed;
+	}
+	
+	public void setMaximumRed(int maximumRed) {
+		this.maximumRed = maximumRed;
+	}
+	
+	public int getMaximumBlue() {
+		return maximumBlue;
+	}
+	
+	public void setMaximumBlue(int maximumBlue) {
+		this.maximumBlue = maximumBlue;
+	}
+	
+	
+}
